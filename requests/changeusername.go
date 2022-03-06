000029c0c5be64ad58c032a39b16b81ed337661c
Yann L <yann.lacroix.dev@gmail.com>
2022-03-06 17:24:33 +0100

    Add check to notify peers in room only if the client is in a room

--- requests/changeusername.go
+++ requests/changeusername.go
@@ -32,13 +32,8 @@ func (r ChangeUsernameRequest) Check() error {
 
 // Handles a username changement request from a client.
 func (r ChangeUsernameRequest) Handle(remoteAddr string, conn *websocket.Conn, users *obj.Users, rooms *obj.Rooms) res.Response {
-	// Fetch user and room
+	// Fetch user an rename
 	user, err := users.User(r.Salt, remoteAddr)
-	if err != nil {
-		return res.NewErrorResponse(err.Error())
-	}
-
-	room, err := rooms.Room(user.RoomID)
 	if err != nil {
 		return res.NewErrorResponse(err.Error())
 	}
@@ -48,11 +43,19 @@ func (r ChangeUsernameRequest) Handle(remoteAddr string, conn *websocket.Conn, u
 		return res.NewErrorResponse(err.Error())
 	}
 
-	notifyPeers(rooms, room)
+	// If connected to a room, notify peers of the changement
+	if user.RoomID != "" {
+		room, err := rooms.Room(user.RoomID)
+		if err != nil {
+			return res.NewErrorResponse(err.Error())
+		}
+
+		notifyPeers(rooms, room)
+	}
 
 	log.Println(remoteAddr, "ChangeUsernameRequest success")
 
-	return res.NewResponse(res.SuccessResponse{RequestCode: res.CodeType(r.Code())})
+	return res.NewResponse(res.ChangeUsernameResponse{Username: r.NewUsername})
 }
 
 func (r ChangeUsernameRequest) Code() CodeType {
