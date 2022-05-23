5c6a42bd03abccc148181ed6f362cc73bbe46c72
Fabien D <49648494+WhiteDiamondz@users.noreply.github.com>
2022-05-23 11:56:27 +0200

    Update irequest.go

--- requests/irequest.go
+++ requests/irequest.go
@@ -18,7 +18,7 @@ const (
 	JOIN_ROOM = "JRO"
 	// Follow a JOIN_ROOM request. Grant or deny user access to the room, return nothing.
 	JOIN_ROOM_ANSWER = "JRA"
-	// Forward the message to all peers within the room. The messages are not keeped in the rooms.
+	// Forward the message to all peers within the room. The messages are not kept in the rooms.
 	NEW_MESSAGE = "NME"
 	// Change the username
 	CHANGE_USERNAME = "CHU"
